4fee16e68b20a9db4f9760f941f4c758c01e7efa
Vidiic <jordividal1997@gmail.com>
2020-10-30 14:26:43 +0100

    remove user db

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -4,7 +4,6 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:timtrack/models/activity_model.dart';
-import 'package:timtrack/models/user_model.dart';
 
 class DBProvider {
   static Database _database;
